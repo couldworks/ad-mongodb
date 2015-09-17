b5ca97dc3db523e0fb3b5ad7f8c2169c1ee20336
Scisco <scisco7@gmail.com>
2015-09-17 12:49:35 -0400

    use config.js to add user for landsat-api db

--- 3.0/config.sh
+++ 3.0/config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PASS=${MONGODB_PASS:-$(pwgen -s 12 1)}
+PASS=${MONGODB_PASS:-$(pwgen -s 20 1)}
+LANDSAT_PASSWORD=${LANDSAT_PASSWORD:-$(pwgen -s 20 1)}
+USERNAME=${USERNAME:-landsatuser}
 _word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
 
 RET=1
@@ -12,15 +14,18 @@ while [[ RET -ne 0 ]]; do
 done
 
 echo "=> Creating an admin user with a ${_word} password in MongoDB"
+mongo landsat-api --eval "username='$USERNAME';password='$LANDSAT_PASSWORD';databaseName='landsat-api';" /config.js
 mongo admin --eval "db.createUser({user: 'admin', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
 
+
 echo "=> Done!"
-touch /data/db/.mongodb_password_set
+touch /data/db/.mongo_configured
 
 echo "========================================================================"
 echo "You can now connect to this MongoDB server using:"
 echo ""
 echo "    mongo admin -u admin -p $PASS --host <host> --port <port>"
+echo "    mongo landsat-api -u landsatuser -p $LANDSAT_PASSWORD --host <host> --port <port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "========================================================================"
