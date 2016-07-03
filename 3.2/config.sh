1ea5aace4ce1e1f4b38e3b840d6503e08f0823b9
drr00t <adribeiro@gmail.com>
2016-07-03 16:55:43 -0300

    added support to alpine and version 3.2 of mongodb.

---
+++ 3.2/config.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PASS=${MONGODB_PASS:-$(pwgen -s 20 1)}
+LANDSAT_PASSWORD=${LANDSAT_PASSWORD:-$(pwgen -s 20 1)}
+USERNAME=${USERNAME:-landsatuser}
+_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+echo "=> Creating an admin user with a ${_word} password in MongoDB"
+#mongo landsat-api --eval "username='$USERNAME';password='$LANDSAT_PASSWORD';databaseName='landsat-api';" /config.js
+mongo admin --eval "db.createUser({user: 'admin', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+
+
+echo "=> Done!"
+touch /data/db/.mongo_configured
+
+echo "========================================================================"
+echo "You can now connect to this MongoDB server using:"
+echo ""
+echo "    mongo admin -u admin -p $PASS --host <host> --port <port>"
+echo "    mongo landsat-api -u landsatuser -p $LANDSAT_PASSWORD --host <host> --port <port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
---
+++ 3.2/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -m
+
+mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
+cmd="$mongodb_cmd --httpinterface --rest --master"
+if [ ! -f /data/db/.mongo_configured ]; then
+    $cmd &
+    /config.sh
+    kill $(pgrep mongod)
+fi
+
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
+
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+$cmd #&
+
+#fg $(pgrep mongod)
